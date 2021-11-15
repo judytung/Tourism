b4db8e5d86fe60faa5c75ca0d7e248de0495cf86
TUNG HSIAO HAN <sky250331@gmail.com>
2021-11-15 11:56:35 +0800

    縣市搜尋欄位

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,16 @@
+const regionSearch = document.querySelector('.regionSearch');
+let region = ['基隆市','台北市','新北市','桃園市','新竹市','新竹縣','宜蘭縣','苗栗縣','臺中市','彰化縣','南投縣','雲林縣','嘉義市','嘉義縣','臺南市','高雄市','屏東縣','臺東縣','花蓮縣','金門縣','連江縣','澎湖縣'];
+    let str = `<option value="地區搜尋" disabled selected hidden>請選擇縣市</option>
+  <option value="">全部地區</option>`;
+    region.forEach(function(item) {
+      str += `<option value="${item}" class="regionOption">${item}</option>`
+    })
+                   
+  regionSearch.innerHTML = str;
+
+
+
+
 
 axios.get(
   '',
@@ -12,6 +25,9 @@ axios.get(
  console.log(error);
 }); 
 
+
+
+// API 帶 header 驗證
 function getAuthorizationHeader() {
 //  填入自己 ID、KEY 開始
    let AppID = 'e1aa6d263b8549b796b5299c1eae6f21';
