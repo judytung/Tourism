26265d48ccf511de4abc85d58beac630617ebe41
TUNG HSIAO HAN <sky250331@gmail.com>
2021-11-15 15:18:51 +0800

    Update 2021-11-15T07:18:48.996Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,19 +1,40 @@
 "use strict";
 
 var regionSearch = document.querySelector('.regionSearch');
-var region = ['基隆市', '台北市', '新北市', '桃園市', '新竹市', '新竹縣', '宜蘭縣', '苗栗縣', '臺中市', '彰化縣', '南投縣', '雲林縣', '嘉義市', '嘉義縣', '臺南市', '高雄市', '屏東縣', '臺東縣', '花蓮縣', '金門縣', '連江縣', '澎湖縣'];
-var str = "<option value=\"\u5730\u5340\u641C\u5C0B\" disabled selected hidden>\u8ACB\u9078\u64C7\u7E23\u5E02</option>\n  <option value=\"\">\u5168\u90E8\u5730\u5340</option>";
-region.forEach(function (item) {
-  str += "<option value=\"".concat(item, "\" class=\"regionOption\">").concat(item, "</option>");
+var scenicList = document.querySelector('.scenicList');
+var searchBtn = document.querySelector('.searchBtn');
+
+function init() {
+  var region = ['基隆市', '台北市', '新北市', '桃園市', '新竹市', '新竹縣', '宜蘭縣', '苗栗縣', '臺中市', '彰化縣', '南投縣', '雲林縣', '嘉義市', '嘉義縣', '臺南市', '高雄市', '屏東縣', '臺東縣', '花蓮縣', '金門縣', '連江縣', '澎湖縣'];
+  var str = "<option value=\"\u5730\u5340\u641C\u5C0B\" disabled selected hidden>\u8ACB\u9078\u64C7\u7E23\u5E02</option>\n <option value=\"\">\u5168\u90E8\u5730\u5340</option>";
+  region.forEach(function (item) {
+    str += "<option value=\"".concat(item, "\" class=\"regionOption\">").concat(item, "</option>");
+  });
+  regionSearch.innerHTML = str;
+}
+
+init(); // 渲染
+
+searchBtn.addEventListener('click', function (e) {
+  render();
 });
-regionSearch.innerHTML = str;
-axios.get('', {
-  headers: getAuthorizationHeader()
-}).then(function (response) {
-  document.querySelector('body').textContent = JSON.stringify(response.data);
-})["catch"](function (error) {
-  console.log(error);
-}); // API 帶 header 驗證
+
+function render() {
+  axios.get('https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?$select=Name%2CAddress%2COpenTime%2CTicketInfo%2CPicture&$format=JSON', {
+    headers: getAuthorizationHeader()
+  }).then(function (response) {
+    // document.querySelector('body').textContent=JSON.stringify(response.data);
+    var data = response.data;
+    var str = '';
+    data.forEach(function (item) {
+      str += "<li class=\"d-flex py-2 border-bottom\">\n               <div class=\"me-ml\">\n                  <img src=\"".concat(item.Picture.PictureUrl1, "\" alt=\"").concat(item.Picture.PictureDescription1, "\" class=\"rounded-2 h-6.125 w-6.125\">\n               </div>\n               <div>\n                  <h2 class=\"fs-3 mb-xs\">").concat(item.Name, "</h2>\n                  <div class=\"mb-xs fs-2 d-flex\">\n                     <span class=\"material-icons-outlined\">location_on</span>\n                     <p>").concat(item.Address, "</p>\n                  </div>\n                  <div class=\"mb-xs fs-2 d-flex\">\n                     <span class=\"material-icons-outlined\">schedule</span>\n                     <p>").concat(item.OpenTime, "</p>\n                  </div>\n                  <div class=\"fs-2 d-flex\">\n                     <span class=\"material-icons-outlined\">attach_money</span>\n                     <p>").concat(item.TicketInfo, "</p>\n                  </div>\n               </div>\n         </li>");
+    });
+    scenicList.innerHTML = str;
+  })["catch"](function (error) {
+    console.log(error);
+  });
+} // API 帶 header 驗證
+
 
 function getAuthorizationHeader() {
   //  填入自己 ID、KEY 開始
