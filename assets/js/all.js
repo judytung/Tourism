9e3b7f902dc23f78b16f28e6716f72a86e2af7ea
TUNG HSIAO HAN <sky250331@gmail.com>
2021-11-15 11:57:08 +0800

    Update 2021-11-15T03:57:05.784Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,12 +1,19 @@
 "use strict";
 
+var regionSearch = document.querySelector('.regionSearch');
+var region = ['基隆市', '台北市', '新北市', '桃園市', '新竹市', '新竹縣', '宜蘭縣', '苗栗縣', '臺中市', '彰化縣', '南投縣', '雲林縣', '嘉義市', '嘉義縣', '臺南市', '高雄市', '屏東縣', '臺東縣', '花蓮縣', '金門縣', '連江縣', '澎湖縣'];
+var str = "<option value=\"\u5730\u5340\u641C\u5C0B\" disabled selected hidden>\u8ACB\u9078\u64C7\u7E23\u5E02</option>\n  <option value=\"\">\u5168\u90E8\u5730\u5340</option>";
+region.forEach(function (item) {
+  str += "<option value=\"".concat(item, "\" class=\"regionOption\">").concat(item, "</option>");
+});
+regionSearch.innerHTML = str;
 axios.get('', {
   headers: getAuthorizationHeader()
 }).then(function (response) {
   document.querySelector('body').textContent = JSON.stringify(response.data);
 })["catch"](function (error) {
   console.log(error);
-});
+}); // API 帶 header 驗證
 
 function getAuthorizationHeader() {
   //  填入自己 ID、KEY 開始
