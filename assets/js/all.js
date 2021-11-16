72439a72284415c747990951b99ff076d2eb2863
TUNG HSIAO HAN <sky250331@gmail.com>
2021-11-16 12:10:28 +0800

    Update 2021-11-16T04:10:24.430Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,17 +3,60 @@
 var regionSearch = document.querySelector('.regionSearch');
 var scenicList = document.querySelector('.scenicList');
 var searchBtn = document.querySelector('.searchBtn');
+var region = ['基隆市', '台北市', '新北市', '桃園市', '新竹市', '新竹縣', '宜蘭縣', '苗栗縣', '臺中市', '彰化縣', '南投縣', '雲林縣', '嘉義市', '嘉義縣', '臺南市', '高雄市', '屏東縣', '臺東縣', '花蓮縣', '金門縣', '連江縣', '澎湖縣'];
 
 function init() {
-  var region = ['基隆市', '台北市', '新北市', '桃園市', '新竹市', '新竹縣', '宜蘭縣', '苗栗縣', '臺中市', '彰化縣', '南投縣', '雲林縣', '嘉義市', '嘉義縣', '臺南市', '高雄市', '屏東縣', '臺東縣', '花蓮縣', '金門縣', '連江縣', '澎湖縣'];
-  var str = "<option value=\"\u5730\u5340\u641C\u5C0B\" disabled selected hidden>\u8ACB\u9078\u64C7\u7E23\u5E02</option>\n <option value=\"\">\u5168\u90E8\u5730\u5340</option>";
+  var str = "<option value=\"\" disabled hidden selected>\u8ACB\u9078\u64C7\u7E23\u5E02</option>\n <option value=\"\u5168\u90E8\u5730\u5340\">\u5168\u90E8\u5730\u5340</option>";
   region.forEach(function (item) {
     str += "<option value=\"".concat(item, "\" class=\"regionOption\">").concat(item, "</option>");
   });
   regionSearch.innerHTML = str;
 }
 
-init(); // 渲染
+init(); // 地區篩選
+// regionSearch.addEventListener('change', function (e) {
+//    const area = e.target.value;
+//    // console.log(area);
+//    axios.get(
+//       `https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?$filter=contains(Name,'${area}')&$format=JSON`,
+//       {
+//          headers: getAuthorizationHeader()
+//       }
+//    )
+//    .then(function (response) {
+//       searchBtn.addEventListener('click', function (e) {
+//          const data = response.data;
+//          let str = '';
+//          data.forEach(function(item){
+//             str += `<li class="d-flex py-2 border-bottom overflow-hidden">
+//                   <div class="me-ml">
+//                      <img src="${item.Picture.PictureUrl1}" alt="${item.Picture.PictureDescription1}" class="rounded-2 h-6.125 w-6.125奧奧">
+//                   </div>
+//                   <div>
+//                      <h2 class="fs-3 mb-xs">${item.Name}</h2>
+//                      <div class="mb-xs fs-2 d-flex">
+//                         <span class="material-icons-outlined">location_on</span>
+//                         <p>${item.Address}</p>
+//                      </div>
+//                      <div class="mb-xs fs-2 d-flex">
+//                         <span class="material-icons-outlined">schedule</span>
+//                         <p>${item.OpenTime}</p>
+//                      </div>
+//                      <div class="fs-2 d-flex">
+//                         <span class="material-icons-outlined">attach_money</span>
+//                         <p>${item.TicketInfo}</p>
+//                      </div>
+//                   </div>
+//             </li>`
+//          })
+//          scenicList.innerHTML = str;
+//       })
+//    })
+//    .catch(function (error) {
+//      console.log(error);
+//    }); 
+// })
+// 渲染
 
 searchBtn.addEventListener('click', function (e) {
   render();
@@ -27,7 +70,7 @@ function render() {
     var data = response.data;
     var str = '';
     data.forEach(function (item) {
-      str += "<li class=\"d-flex py-2 border-bottom\">\n               <div class=\"me-ml\">\n                  <img src=\"".concat(item.Picture.PictureUrl1, "\" alt=\"").concat(item.Picture.PictureDescription1, "\" class=\"rounded-2 h-6.125 w-6.125\">\n               </div>\n               <div>\n                  <h2 class=\"fs-3 mb-xs\">").concat(item.Name, "</h2>\n                  <div class=\"mb-xs fs-2 d-flex\">\n                     <span class=\"material-icons-outlined\">location_on</span>\n                     <p>").concat(item.Address, "</p>\n                  </div>\n                  <div class=\"mb-xs fs-2 d-flex\">\n                     <span class=\"material-icons-outlined\">schedule</span>\n                     <p>").concat(item.OpenTime, "</p>\n                  </div>\n                  <div class=\"fs-2 d-flex\">\n                     <span class=\"material-icons-outlined\">attach_money</span>\n                     <p>").concat(item.TicketInfo, "</p>\n                  </div>\n               </div>\n         </li>");
+      str += "<li class=\"d-flex py-2 border-bottom overflow-hidden\">\n                  <div class=\"me-ml\">\n                     <img src=\"".concat(item.Picture.PictureUrl1, "\" alt=\"").concat(item.Picture.PictureDescription1, "\" class=\"rounded-2 h-6.125 w-6.125\">\n                  </div>\n                  <div>\n                     <h2 class=\"fs-3 mb-xs\">").concat(item.Name, "</h2>\n                     <div class=\"mb-xs fs-2 d-flex\">\n                        <span class=\"material-icons-outlined\">location_on</span>\n                        <p>").concat(item.Address, "</p>\n                     </div>\n                     <div class=\"mb-xs fs-2 d-flex\">\n                        <span class=\"material-icons-outlined\">schedule</span>\n                        <p>").concat(item.OpenTime, "</p>\n                     </div>\n                     <div class=\"fs-2 d-flex\">\n                        <span class=\"material-icons-outlined\">attach_money</span>\n                        <p>").concat(item.TicketInfo, "</p>\n                     </div>\n                  </div>\n            </li>");
     });
     scenicList.innerHTML = str;
   })["catch"](function (error) {
